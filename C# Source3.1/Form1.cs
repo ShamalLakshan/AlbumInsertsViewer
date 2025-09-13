ce523b99f3a6f6d22f7c549d507a096ff0c1703e
Shamal Lakshan <shamallakshan00600@gmail.com>
2025-09-13 11:39:43 +0530

    image viewer/slidshow

--- C# Source3.1/Form1.cs
+++ C# Source3.1/Form1.cs
@@ -7,39 +7,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MusicBeePlugin
 {
     public partial class Form1 : Form
     {
+        int counter = 0;
+        // Hardcoded directory path - change this to your desired directory
+        string imageDirectory = @"E:\Github\AlbumInsertsViewer\TestCoverArts";
+        string[] images;
+        bool playing = false;
+
         public Form1()
         {
             InitializeComponent();
-            //webBrowser1.Url = new Uri("E:\\booklet.pdf");
-            
-            // Image List Usage
-            //Graphics g = Graphics.FromHwnd(Handle);
-            //imageList1.ColorDepth = ColorDepth.Depth32Bit;
-            //imageList1.ImageSize = new Size(255, 255);
 
-            //imageList1.Images.Add(Image.FromFile(@"E:\booklet_01.jpg"));
-            //imageList1.Images.Add(Image.FromFile(@"E:\CD3-5-SleepingBeauty-front.jpg"));
-            //imageList1.Images.Add(Image.FromFile(@"E:\CD6_CD7-Nutcracker-front.jpg"));
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
 
-            //for (int i = 0; i < imageList1.Images.Count;i++)
-            //{
-            //    imageList1.Draw(g, new Point(40, 40), i);
-            //    System.Threading.Thread.Sleep(2000);
-            //}
+            // Load images from hardcoded directory at startup
+            LoadImagesFromDirectory();
 
-            //Use picture box()
-
+            if (!playing)
+            {
+                if (images == null || images.Length == 0)
+                {
+                    MessageBox.Show("No images available to display.");
+                    return;
+                }
+                timer1.Start();
+                playing = true;
+            }
+            else
+            {
+                playing = false;
+                timer1.Stop();
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void LoadImagesFromDirectory()
         {
-            lblState.ForeColor = Color.Red;
+            try
+            {
+                // Get all image files from the hardcoded directory
+                string[] extensions = { "*.jpg", "*.jpeg", "*.png", "*.bmp", "*.gif" };
+                List<string> imageFiles = new List<string>();
+
+                foreach (string extension in extensions)
+                {
+                    imageFiles.AddRange(Directory.GetFiles(imageDirectory, extension, SearchOption.TopDirectoryOnly));
+                }
+
+                images = imageFiles.ToArray();
+
+                // Display first image if any images found
+                if (images.Length > 0)
+                {
+                    pictureBox1.Image = System.Drawing.Image.FromFile(images[0]);
+                }
+                else
+                {
+                    MessageBox.Show($"No images found in directory: {imageDirectory}");
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show($"Directory not found: {imageDirectory}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading images: {ex.Message}");
+            }
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (images != null && images.Length > 0)
+            {
+                counter++;
+                if (counter >= images.Length)
+                {
+                    counter = 0;
+                }
+
+                try
+                {
+                    pictureBox1.Image?.Dispose(); // Dispose previous image to prevent memory leaks
+                    pictureBox1.Image = System.Drawing.Image.FromFile(images[counter]);
+                    //filename.Text = Path.GetFileName(images[counter]); //show name of the image
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading image: {ex.Message}");
+                }
+            }
+        }
     }
 }
